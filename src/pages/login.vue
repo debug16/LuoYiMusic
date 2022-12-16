829b4152835435bee2db231be32127bf8f41216c
debug16 <1620014532@qq.com>
2022-12-16 22:44:50 +0800

    添加 快捷键展示

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -13,7 +13,7 @@
       <div class="forgetPass">
         <a href="#">忘记密码</a>
       </div>
-      <div class="login__submit" color="#fff" bg="[hsla(248,70%,57%,1)]" hover="bg-[hsla(248,80%,57%,1)] cursor-pointer" active:transform="scale-101" w-full text-center p-2 rounded-lg>登录</div>
+      <div class="login__submit" shadow-lg shadow="[hsla(248,50%,57%,1)]" color="#fff" bg="[hsla(248,70%,57%,1)]" hover="bg-[hsla(248,80%,57%,1)] cursor-pointer" active:transform="scale-101" w-full text-center p-2 rounded-lg>登录</div>
     </div>
     <div class="icon"></div>
   </div>
@@ -25,8 +25,8 @@
   .forgetPass {
     margin-top: 6px !important;
     @apply self-end text-sm color-[hsla(248,40%,57%,1)];
-    &:hover{
-    @apply color-[hsla(248,65%,57%,1)] underline;
+    &:hover {
+      @apply color-[hsla(248,65%,57%,1)] underline;
     }
   }
 }
