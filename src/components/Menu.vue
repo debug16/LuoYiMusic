c95c26753c231f0398242b1cc384dd70b0f9d732
debug16 <1620014532@qq.com>
2022-12-19 00:35:26 +0800

    修复 弹出框超出边界

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -11,17 +11,52 @@ const closeMenu = () => {
 
 // 打开菜单
 const openMenu = (event: MouseEvent) => {
-  showMenu = true
+  const { x, y } = keepInBounds(menu, event.clientX, event.clientY)
   nextTick(() => {
     // 使菜单聚焦
     menu.focus()
   })
-  left = `${event.clientX}px`
-  top = `${event.clientY}px`
-
+  
+  left = `${x}px`
+  top = `${y}px`
+  showMenu = true
   event.preventDefault()
 }
 
+// 弹出边界控制
+function keepInBounds(popup: HTMLElement, x: number, y: number) {
+  // 获取弹出框的宽度和高度
+  const popupWidth = popup.offsetWidth
+  const popupHeight = popup.offsetHeight
+  console.log(1,popupHeight,popupWidth);
+  
+
+  // 获取浏览器窗口的内部宽度和高度
+  const windowWidth = window.innerWidth
+  const windowHeight = window.innerHeight
+
+  // 如果弹出框超出了右边界，将其移回合法范围内
+  if (x + popupWidth > windowWidth) {
+    x = windowWidth - popupWidth - 60
+  }
+
+  // 如果弹出框超出了下边界，将其移回合法范围内
+  if (y + popupHeight > windowHeight) {
+    y = windowHeight - popupHeight 
+  }
+
+  // 如果弹出框超出了左边界，将其移回合法范围内
+  if (x < 0) {
+    x = 0
+  }
+
+  // 如果弹出框超出了上边界，将其移回合法范围内
+  if (y < 0) {
+    y = 0
+  }
+
+  return { x, y }
+}
 defineExpose({
   openMenu,
 })
