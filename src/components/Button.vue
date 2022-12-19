a1ed0d485d9d80a39a293fcec78d349e423888cc
debug16 <1620014532@qq.com>
2022-12-20 01:00:53 +0800

    style:处理样式

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -9,7 +9,7 @@ const { color = '#335eea', bg = '#8caaff73' } = defineProps<Props>()
 </script>
 
 <template>
-  <div class="button" :bg="bg" :color="color" select-none cursor-pointer flex="~" items-center space-x-1 py-2 px-4 rounded-lg font-500 hover:scale-104 transition-transform>
+  <div class="button" :bg="bg" :color="color" select-none cursor-pointer flex="~" items-center space-x-1 py-2 px-4 rounded-lg font-500 text-base hover:scale-104 transition-transform>
     <slot name="icon"></slot>
     <span v-if="text">{{ text }}</span>
   </div>
