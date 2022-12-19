6a93a485a1fccd2f2190409023c70b7db030743d
debug16 <1620014532@qq.com>
2022-12-20 04:18:00 +0800

    components: 添加播放按钮组件

---
+++ src/components/IconPlay.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="icon-play" w-full h-full  hidden>
+    <div class="play__btn " w-full h-full flex justify-center items-center bg="#fff/20" backdrop-blur-lg hover="bg-#fff/30 backdrop-blur-lg" rounded="50%">
+      <div i-mingcute-play-fill w="58%" h="58%" bg="#fff" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
