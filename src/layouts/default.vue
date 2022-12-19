3890bac41b305a217db311c251117f7e9d9ae57a
debug16 <1620014532@qq.com>
2022-12-19 15:05:16 +0800

    fix: 无法展开全屏歌词页

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -19,7 +19,7 @@ provide('isFullScreenPlayer', { isFullScreenPlayer, updateIsFullScreenPlayer })
 
 <template>
   <!-- 头部内容 -->
-  <div class="header" h-65px backdrop-blur-xl bg="#fff/90">
+  <div class="header" h-65px >
     <Header />
   </div>
   <!-- 主体内容 -->
@@ -29,12 +29,12 @@ provide('isFullScreenPlayer', { isFullScreenPlayer, updateIsFullScreenPlayer })
     </RouterView>
   </main>
   <!-- 尾部内容 -->
-  <div :class="{ slideUp: footerHide }" transition-transform transition-duration-400 backdrop-blur-xl class="footer" bg="#fff/85">
+  <div :class="{ slideUp: footerHide }" transition-transform transition-duration-400  class="footer">
     <Footer />
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 main {
   height: calc(100vh);
   overflow-y: auto;
@@ -48,6 +48,11 @@ main {
   z-index: 99;
 }
 
+.header::before,
+.footer::before{
+  content: '';
+  @apply w-full h-full bg-#fff/85 backdrop-blur-xl absolute top-0 left-0 -z1;
+}
 .header {
   top: 0;
 }
