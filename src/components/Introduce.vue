05a6c647dd75c94fa14c00e3d3f25dc6fe1b6d3e
debug16 <1620014532@qq.com>
2022-12-19 22:56:55 +0800

    component: add组件

---
+++ src/components/Introduce.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+interface Props {
+  name: string,
+  title?:string,
+  titleContent?:string,
+  description?: string
+}
+const { name } = defineProps<Props>()
+</script>
+
+<template>
+  <div class="introduce" flex="~"> 
+    <div class="left" shrink-0 w="1/5" mr-15>
+      <!-- slot left -->
+      <slot name="left" />
+    </div>
+    <div class="songsContent" font-400 flex="~ col" justify-evenly items-start min-h-full>
+      <div class="songsName" text-3xl font-900>
+        {{ name }}
+      </div>
+      <div class="singer" text-lg>
+        <p text-lg>{{title}}</p>
+        <p class="time" color="#000/60" text-sm>{{titleContent}}</p>
+      </div>
+      <div class="describe" text-sm color="#000/60" :title="description">
+        {{ description }}
+      </div>
+      <!-- slot footer -->
+        <div class="footer">
+        <slot name="footer" />
+      </div>
+    </div>
+    <slot/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.introduce {
+  .describe {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
+  }
+}
+</style>
