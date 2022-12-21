40f4fb53505337f153bd023ccd8062cedb7b391f
debug16 <1620014532@qq.com>
2022-12-22 04:42:36 +0800

    to: 发现一个 bug 待修复

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -331,6 +331,7 @@ watchEffect(() => {
   // ESC键 取消歌词全屏
   if (Escape) isFullScreenPlayer = false
   // F键(full) 歌词全屏切换
+  // TODO 需要判断是否有播放的音乐 不然先按 f 键后再播放音乐会直接弹出歌词全屏。 做了判断 但是不知道为什么 依赖注入 更新状态很慢 会要很长时间才更新 所以会导致f键失效
   if (f) isFullScreenPlayer = !isFullScreenPlayer
 })
 
