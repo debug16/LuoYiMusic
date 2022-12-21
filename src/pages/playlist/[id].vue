b1556da9f7d21e5399264a6e9b641e43e2ad4398
debug16 <1620014532@qq.com>
2022-12-22 02:58:01 +0800

    refactor: 优化

--- src/pages/playlist/[id].vue
+++ src/pages/playlist/[id].vue
@@ -70,10 +70,6 @@ const imgUrl = (url: string, param?: number | string) => {
   const img = url ? url + param : ''
   return img
 }
-
-const timeFormat = (time:number)=>{
-  return Math.floor(time / 1000 / 60) + ':' + `${Math.floor((time / 1000) % 60)}`.padStart(2, '0')
-}
 </script>
 
 <template>
@@ -111,39 +107,6 @@ const timeFormat = (time:number)=>{
         :img-src="imgUrl(songs?.al.picUrl, 224)"
         @songs-dblclick="dblclickPlayMusic(songs)"
       ></SongsInfo>
-      <!-- <div
-        v-for="songs in songsList"
-        :key="songs.id"
-        w-full
-        flex="~"
-        p-2
-        rounded-xl
-        items-center
-        :class="{ vip: songs.fee !== 0 && songs.fee !== 8, active: songs.id == playMusicStore.getPlayMusicId }"
-        :title="songs.fee !== 0 && songs.fee !== 8 ? 'Only VIP Can Play' : ''"
-        @dblclick="dblclickPlayMusic(songs)"
-      >
-        <div class="songs" flex="~ grow-0" items-center w="50%" pr-5>
-          <div h-13 w-13 shrink-0 mr-5>
-            <img h-full rounded-lg :src="imgUrl(songs?.al.picUrl, 224)" alt="" />
-          </div>
-          <div overflow-hidden>
-            <div truncate grow-0 w-full font-800 text-lg>
-              <span>{{ songs.name }}</span>
-              <span v-if="songs.alia[0]" text="#7a7a7a/60"> ({{ songs?.alia && songs?.alia[0]?.toString() }}) </span>
-            </div>
-            <div text-xs color="#000/80" truncate>
-              <span>{{ formatSongsSinger(songs?.ar) }}</span>
-            </div>
-          </div>
-        </div>
-        <div basis="40%" font-bold text-sm>
-          <span>{{ songs.al.name }}</span>
-        </div>
-        <div basis="10%" text-right>
-          <span>{{ Math.floor(songs.dt / 1000 / 60) }}:{{ `${Math.floor((songs.dt / 1000) % 60)}`.padStart(2, '0') }}</span>
-        </div>
-      </div> -->
     </div>
   </div>
 </template>
