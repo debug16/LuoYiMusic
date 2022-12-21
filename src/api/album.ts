38d7fedffb144dd65feb960f3f2a9f89201b9150
debug16 <1620014532@qq.com>
2022-12-22 04:43:25 +0800

    feat: 新增专辑页

--- src/api/album.ts
+++ src/api/album.ts
@@ -2,10 +2,17 @@ import request from '~/utils/request'
 
 /**
  * 获取歌手的专辑
- * @param id 歌手id
+ * @param id 歌手 id
  * @param limit 取出数量 默认为30
  */
 export const artistAlbum = (id: number | string, limit: number = 30) => {
   return request.get('artist/album', { params: { id, limit } })
-  return request()
+}
+
+/**
+ * 获取专辑内容
+ * @param id 专辑 id
+ */
+export const album = (id: number | string) => {
+  return request.get('album', { params: { id } })
 }
