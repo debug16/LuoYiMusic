11bc442cf2abcc9d02e07a83736e1690b776d86f
debug16 <1620014532@qq.com>
2022-12-22 10:31:23 +0800

    fix: 修复默认图片展示比例问题 替换其他图片

--- src/utils/img.ts
+++ src/utils/img.ts
@@ -4,8 +4,14 @@
  * @param param 大小
  * @returns 
  */
-export const imgUrlSize = (url: string, param?: number | string) => {
-  param = param ? `?param=${param}y${param}` : ''
-  const img = url ? url + param : ''
+export const imgUrlSize = (url: string | undefined, param?: number | undefined) => {
+  if(!url) return '';
+  const start = url.lastIndexOf('/')
+  const end = url.lastIndexOf('.')
+
+  const flag = url.slice(start+1,end) === '5639395138885805'
+  if(flag) return `https://p2.music.126.net/VnZiScyynLG7atLIZ2YPkw==/18686200114669622.jpg?param=${param}y${param}`
+  const urlParam = param ? `?param=${param}y${param}` : ''
+  const img = url ? url + urlParam : ''
   return img
 }
