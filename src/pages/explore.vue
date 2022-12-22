b2c512cf593281ee5ff85dbddc3fcc8294f19812
debug16 <1620014532@qq.com>
2022-12-23 05:12:31 +0800

    feat: 新增发现页

--- src/pages/explore.vue
+++ src/pages/explore.vue
@@ -1,26 +1,64 @@
 <script setup lang="ts">
-import { onBeforeRouteUpdate, Router } from 'vue-router';
+import { playlistDetail, playlistHighqualityTags, topPlaylistHighquality } from '~/api/playlist'
+import { onBeforeRouteUpdate } from 'vue-router'
+import { imgUrlSize } from '~/utils/img'
+import { formatDate } from '~/utils/date'
+import { isSongsFree } from '~/utils/songs';
+import { usePlayMusicStore } from '~/stores/playMusic';
 
-const categoryList: Array<string> = [
-  "全部",
-  "推荐歌单",
-  "精品歌单",
-  "官方",
-  "排行榜",
-  "欧美",
-  "流行",
-  "摇滚",
-  "电子",
-  "说唱",
-  "ACG",
-  "· · ·"
-]
+// const categoryList: Array<string> = ['全部', '推荐歌单', '精品歌单', '官方', '排行榜', '欧美', '流行', '摇滚', '电子', '说唱', 'ACG', '· · ·']
+const playMusicStore = usePlayMusicStore()
+const router: any = useRouter()
+const route: any = useRoute()
 
-const router: Router = useRouter();
+let tags: any = $ref()
+
+let playList: any = $ref()
 
 
-onBeforeRouteUpdate((route) => {
-  // console.log(route.to)
+// 获取歌单事件
+const onPlayRecommendedList = (playlistId: number) => {
+  // 获取歌单详情
+  playlistDetail(playlistId).then(res => {
+    let playlist: Array<any> = []
+
+    if (res.code === 200) playlist = res.playlist?.tracks
+
+    // 如果歌单列表里没有音乐
+    if (playlist?.length <= 0) return
+    // 找到免费的歌曲
+    const song = playlist.find((songs: { fee: number; id: any }) => {
+      if (isSongsFree(songs.fee)) {
+        playMusicStore.setPlayMusicId(songs.id)
+        playMusicStore.setPlayMusic(songs)
+        return true
+      }
+      return false
+    })
+    // 如果歌单列表里有免费音乐 就放入待播放的音乐列表
+    if (song) playMusicStore.setPlayMusicList(playlist)
+  })
+}
+
+onBeforeMount(() => {
+  playlistHighqualityTags().then((res: any) => {
+    if (res.code === 200) {
+      tags = res.tags
+    }
+  })
+  topPlaylistHighquality().then((res: any) => {
+    if (res.code === 200) {
+      playList = res.playlists
+    }
+  })
+})
+
+onBeforeRouteUpdate(route => {
+  topPlaylistHighquality({ cat: route.query.category }).then((res: any) => {
+    if (res.code === 200) {
+      playList = res.playlists
+    }
+  })
 })
 </script>
 
@@ -32,33 +70,55 @@ onBeforeRouteUpdate((route) => {
     </div>
     <div class="category" mb-6>
       <ul flex flex-wrap>
-        <li v-for="category,i in categoryList" color='#7a7a7b' :key="i" mb-3 bg='#eee/65' hover="color-#335eea bg-#eaeffd" px4 py2 rd-3 mr4 cursor="pointer"
-          @click="router.push(`/explore?category=${category}`)">
-          <span inline-block text-lg  font-600>{{category}}</span>
+        <template v-for="tag in tags"
+          :key="tag.id">
+          <li
+          class="tag"
+          v-if="tag.name !== 'R&B/Soul'"
+          :class="{ active: route.query.category === tag.name }"
+          color="#7a7a7b"
+          mb-3
+          bg="#eee/65"
+          hover="color-#335eea bg-#eaeffd"
+          px4
+          py2
+          rd-3
+          mr4
+          cursor="pointer"
+          @click="router.push(`/explore?category=${tag.name}`)"
+        >
+          <span inline-block text-lg font-600>{{ tag.name }}</span>
         </li>
+        </template>
+        
       </ul>
     </div>
     <div class="recommend__content grid grid-cols-5 grid-flow-row gap-x-4 gap-y-8">
-      <div v-for="i in 20" :key="i" cursor-pointer>
-        <div class="frontCover" relative>
-          <img
-            src="https://p1.music.126.net/UCRS2tO2tZbkGmq6rj4l0A==/109951165491046990.jpg?param=512y512?param=512y512"
-            rounded-xl alt="item.name">
-          <!-- <div class="play__btn -translate-1/2" bg="#fff/20" hidden hover="bg-#fff/30" p-3 rounded="50%" absolute top="1/2" left="1/2" @click.stop="null">
-              <div i-carbon:play-filled-alt w-8 h-8 />
-            </div> -->
-          <IconPlay />
-        </div>
-        <div class="describe" color="#000" title="「雨之摇滚·倾听爱与自由乐章」">
-          「雨之摇滚·倾听爱与自由乐章」
-        </div>
-      </div>
+      <FrontCover 
+      v-for="item in playList" 
+      :key="item.id" cursor-pointer 
+      :src="imgUrlSize(item.coverImgUrl, 350)" 
+      :title="item.name" 
+      :describe="formatDate(item.createTime)"
+      @click-img="router.push(`/playlist/${item.id}`)"
+      @click-title="router.push(`/playlist/${item.id}`)"
+      @click-describe="router.push(`/playlist/${item.id}`)"
+      @click-play="onPlayRecommendedList(item.id)"
+      > 
+    </FrontCover>
     </div>
   </div>
 </template>
 
-<style scoped>
-.frontCover:hover>.play__btn {
-  display: block;
+<style scoped lang="scss">
+.front-cover {
+  &:deep(.title) {
+    @apply line-clamp-1;
+  }
+}
+.category .tag {
+  &.active {
+    @apply color-#335eea bg-#eaeffd;
+  }
 }
 </style>
